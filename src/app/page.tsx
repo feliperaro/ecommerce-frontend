2383501fc2b6f58c58a1d85122fcf152358ffc63
FehRoque <feliperamosroque@gmail.com>
2024-01-13 18:44:04 -0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import variables from "./styles/variables.module.scss";
 import "./styles/main.scss";
-import Login from "./components/login";
 
 export default function Home() {
   return (
     <body>
       <header>
-        <h1>Bem-vindo</h1>
+        <h1>Home</h1>
       </header>
       <main></main>
       <footer></footer>
