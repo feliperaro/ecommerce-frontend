06f2738d56722c8ad2e46eb9c622073b51ee53f6
FehRoque <feliperamosroque@gmail.com>
2024-01-22 14:48:01 -0300

    SignIn and SingUp page components

--- src/app/components/SignIn/index.tsx
+++ src/app/components/SignIn/index.tsx
@@ -1,31 +1,8 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-export default function Example() {
+export default function SignIn() {
     return (
       <>
-        {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full bg-white">
-          <body class="h-full">
-          ```
-        */}
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <span>LOGO</span>
             <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               Sign in to your account
             </h2>
@@ -83,9 +60,8 @@ export default function Example() {
             </form>
   
             <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Start a 14 day free trial
+              <a href="/sign-up" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+                {"Don't have an account? Sign Up"}
               </a>
             </p>
           </div>
