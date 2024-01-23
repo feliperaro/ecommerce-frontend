81b7ac8549cee13347124ec1a14ade6b4b72f1e8
FehRoque <feliperamosroque@gmail.com>
2024-01-23 17:23:04 -0300

    page.tsx reading data from data.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,43 +2,24 @@
 
 import Header from "./components/Header";
 import ProductItem from "./components/ProductItem";
-import { Product } from "./types/product";
-
-import image from "../../public/images/black-tshirt.png";
 import Footer from "./components/Footer";
 
-const products: Array<Product> = [
-  {
-    id: 1,
-    title: "Camisa",
-    description: "camisa description",
-    price: 100,
-    thumbSrc: image,
-    thumbAlt: "ALt image",
-  },
-  {
-    id: 2,
-    title: "Camiseta Confortável",
-    description: "camisa description",
-    price: 50,
-    thumbSrc: image,
-    thumbAlt: "ALt image",
-  },
-];
+import data from "../../public/data.json";
+
 
 export default function Home() {
   return (
-    <body>
+    <body className="font-sans">
       <Header />
-      <main>
+      <main >
+        <div className="flex justify-center">
+          <h3 className="font-bold text-lg">{"Product Lists"}</h3>
+        </div>
         <div className="mt-5">
-          {products.map((product, index) => {
+          {data.products.map((product, index) => {
             return (
               <div key={index} className="border rounded-lg mb-3">
-                <ProductItem
-                  product={product}
-                  href={"#"}
-                />
+                <ProductItem product={product} href={`/product/${product.id}`} />
               </div>
             );
           })}
